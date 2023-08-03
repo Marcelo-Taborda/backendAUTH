44e0e11109fd28da72a5042d4760365409d32410
Marcelo Taborda <86986422+Marcelo-Taborda@users.noreply.github.com>
2023-08-03 16:55:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: 'http://localhost:5173',
-    optionsSuccessStatus: 200,
-    credentials: true
-}, 
-{
-    origin: 'http://localhost:7000',
-    optionsSuccessStatus: 200,
-    credentials: true
-},           
+app.use(cors(         
 {
     origin: 'https://frontenddd-production.up.railway.app/',
     optionsSuccessStatus: 200,
